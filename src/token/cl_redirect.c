d663a4b26cbbad064a2705beeb8107cf7c78f63c
alies_a <arnaud.alies@epitech.eu>
2016-03-18 15:12:12 +0100

    cl

--- src/token/cl_redirect.c
+++ src/token/cl_redirect.c
@@ -5,21 +5,43 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Fri Mar 18 13:52:44 2016 alies_a
-** Last update Fri Mar 18 14:47:11 2016 alies_a
+** Last update Fri Mar 18 15:11:33 2016 alies_a
 */
 
 #include <stdlib.h>
 #include "mysh.h"
 #include "my.h"
 
+static int	cl_redirect(t_token *tok,
+			    char type)
+{
+  char		*buff;
+  int		x;
+
+  x = 0;
+  while (tok->str[x] != '\0' &&
+	 (is_delimiter(tok->str[x]) ||
+	  tok->str[x] == type))
+    {
+      x += 1;
+    }
+  buff = tok->str;
+  if ((tok->str = my_strdup(tok->str + x)) == NULL)
+    return (1);
+  free(buff);
+  return (0);
+}
+
 int	cl_right_simple(t_token *tok)
 {
-  (void)tok;
+  if (cl_redirect(tok, '>'))
+    return (1);
   return (0);
 }
 
 int     cl_left_simple(t_token *tok)
 {
-  (void)tok;
+  if (cl_redirect(tok, '<'))
+    return (1);
   return (0);
 }
