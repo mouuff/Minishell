2cd6fc4fb34a35bb18445e70be329b7216bcf7c0
alies_a <arnaud.alies@epitech.eu>
2016-04-10 18:50:34 +0200

    fix error message

--- src/exec/redirect.c
+++ src/exec/redirect.c
@@ -5,10 +5,9 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Tue Mar 22 14:39:02 2016 alies_a
-** Last update Sun Apr  3 12:59:45 2016 alies_a
+** Last update Sun Apr 10 18:49:25 2016 alies_a
 */
 
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -24,15 +23,24 @@ t_rdr rdr[] = {
   {NULL, 0},
 };
 
+static void	print_err(const char *path)
+{
+  my_puterr(path);
+  if (isdir(path))
+    my_puterr(": Is a directory.\n");
+  else if (is_something(path))
+    my_puterr(": Permission denied.\n");
+  else
+    my_puterr(": No such file or directory.\n");
+}
+
 int	in_simple(const t_cmp *cmp)
 {
   int	fd;
 
   if ((fd = open((cmp->rd)[IN].file, O_RDONLY)) == -1)
     {
-      my_puterr((cmp->rd)[IN].file);
-      if (errno == EACCES)
-	my_puterr(": Permission denied.\n");
+      print_err((cmp->rd)[IN].file);
       return (1);
     }
   if (dup2(fd, 0) == -1)
@@ -51,11 +59,7 @@ int	out_simple(const t_cmp *cmp)
   if ((fd = open((cmp->rd)[OUT].file, O_WRONLY | O_CREAT | O_TRUNC,
 		 0644)) == -1)
     {
-      my_puterr((cmp->rd)[OUT].file);
-      if (errno == EACCES)
-	my_puterr(": Permission denied.\n");
-      if (errno == EISDIR)
-	my_puterr(": Is a directory.\n");
+      print_err((cmp->rd)[OUT].file);
       return (1);
     }
   if (dup2(fd, 1) == -1)
@@ -75,11 +79,7 @@ int	out_double(const t_cmp *cmp)
 		 O_WRONLY | O_CREAT | O_APPEND,
 		 0644)) == -1)
     {
-      my_puterr((cmp->rd)[OUT].file);
-      if (errno == EACCES)
-	my_puterr(": Permission denied.\n");
-      if (errno == EISDIR)
-	my_puterr(": Is a directory.\n");
+      print_err((cmp->rd)[OUT].file);
       return (1);
     }
   if (dup2(fd, 1) == -1)
